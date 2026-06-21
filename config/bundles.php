dc8689f808d33241568b2036f7ebe61ba8b0a5c7
Marcin Bojarski <mbojarski@icloud.com>
2026-06-21 15:19:49 +0200

    chore: limit WebProfilerBundle to dev environment only

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
 ];
